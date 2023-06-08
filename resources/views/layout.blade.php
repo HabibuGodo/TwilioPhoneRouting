054603b0bd12245bd319cb6da4c423adc8e1f3ff
habibugodo <princehabibu17@gmail.com>
2023-06-08 12:55:37 +0300

    Update on caller id

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -429,7 +429,7 @@
     </div>
 
     {{-- <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> --}}
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="/js/twilio.min.js"></script>
     @yield('scripts')
 </body>
